aeb82f3b0c9c10c0affdf9b51203967ec7b70c0e
Rodrigo Ferreira <rodrigoferreira001@hotmail.com>
2026-01-24 20:05:13 -0300

    feat: Ajusta cores do sample

--- sample-client/src/commonMain/kotlin/dev/catbit/mosaic/sample/App.kt
+++ sample-client/src/commonMain/kotlin/dev/catbit/mosaic/sample/App.kt
@@ -2,8 +2,11 @@ package dev.catbit.mosaic.sample
 
 import androidx.compose.runtime.Composable
 import dev.catbit.mosaic.client.application.MosaicApplication
+import dev.catbit.mosaic.sample.ui.theme.lightColorScheme
 
 @Composable
 fun App() {
-    MosaicApplication()
+    MosaicApplication(
+        colorScheme = lightColorScheme
+    )
 }
